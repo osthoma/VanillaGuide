2e68a833f4eca07383ad1a3c5eff18c4c0a80d7e
Oskar Thoma <thomaoskar@googlemail.com>
2018-04-18 06:17:01 +0200

    consistent naming of quest chains
    for all steps after the first: 'Quest name' (x)

--- GuideTables/001_Introduction.lua
+++ GuideTables/001_Introduction.lua
@@ -5,10 +5,10 @@ Table_001_Introduction = {
 		title = "Introduction",
 		items = {
 			[1] = { str = "     Thank you for using #VIDEOVanilla#Guide!" },
-			[2] = { str = "         written and mantained by #DOQUESTmrmr#." },
+			[2] = { str = "         written and mantained by #QUESTmrmr#." },
 			[3] = { str = "         version #VIDEOv##A_" .. version .. "#" },
 			[4] = { str = "  Originally created for '#VIDEOJ#oana's #VIDEOHorde# Guide', it's grown bigger," },
-			[5] = { str = "with '#DOQUESTB#rianKopps` #DOQUESTAlliance# Guide' by Kira and Cdlp and Profession guides by Velenran." },
+			[5] = { str = "with '#QUESTB#rianKopps` #QUESTAlliance# Guide' by Kira and Cdlp and Profession guides by Velenran." },
 			[6] = { str = "You can find this version somewhere in the internet and if you're reading this....you know where..." },
 			[7] = { str = "." },
 			[8] = { str = "#VIDEODISCLAIMER:#" },
@@ -16,7 +16,7 @@ Table_001_Introduction = {
 			[10] = { str = "#Q_'Name of a Quest'#" },
 
 			[11] = { str = "#I_[Name of an Item]#" },
-			[12] = { str = "." },
+			[12] = { str = "#A_Name of an Area#" },
 			[13] = { str = "#N_Name of a NPC#" },
 			[14] = { str = "#C_Coordinates [XX,XX]#" },
 			[15] = { str = "." },
